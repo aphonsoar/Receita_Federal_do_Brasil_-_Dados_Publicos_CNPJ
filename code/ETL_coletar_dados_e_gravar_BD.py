d003d096ca1053f9e78f0ac10932a4ff8e8ab9ee
italojohnny <italojohnnydosanjos@gmail.com>
2023-02-06 15:41:11 -0300

    Refatora codigo para usar nova funcao

--- code/ETL_coletar_dados_e_gravar_BD.py
+++ code/ETL_coletar_dados_e_gravar_BD.py
@@ -245,7 +245,7 @@ for e in range(0, len(arquivos_empresa)):
 
     # Gravar dados no banco:
     # Empresa
-    empresa.to_sql(name='empresa', con=engine, if_exists='append', index=False)
+    to_sql(empresa, name='empresa', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_empresa[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -325,7 +325,7 @@ for e in range(0, len(arquivos_estabelecimento)):
 
     # Gravar dados no banco:
     # estabelecimento
-    estabelecimento.to_sql(name='estabelecimento', con=engine, if_exists='append', index=False)
+    to_sql(estabelecimento, name='estabelecimento', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_estabelecimento[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -385,7 +385,7 @@ for e in range(0, len(arquivos_socios)):
 
     # Gravar dados no banco:
     # socios
-    socios.to_sql(name='socios', con=engine, if_exists='append', index=False)
+    to_sql(socios, name='socios', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_socios[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -459,7 +459,7 @@ for e in range(0, len(arquivos_simples)):
 
         # Gravar dados no banco:
         # simples
-        simples.to_sql(name='simples', con=engine, if_exists='append', index=False)
+        to_sql(simples, name='simples', con=engine, if_exists='append', index=False)
         print('Arquivo ' + arquivos_simples[e] + ' inserido com sucesso no banco de dados! - Parte '+ str(i+1))
 
         try:
@@ -510,7 +510,7 @@ for e in range(0, len(arquivos_cnae)):
 
     # Gravar dados no banco:
     # cnae
-    cnae.to_sql(name='cnae', con=engine, if_exists='append', index=False)
+    to_sql(cnae, name='cnae', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_cnae[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -555,7 +555,7 @@ for e in range(0, len(arquivos_moti)):
 
     # Gravar dados no banco:
     # moti
-    moti.to_sql(name='moti', con=engine, if_exists='append', index=False)
+    to_sql(moti, name='moti', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_moti[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -600,7 +600,7 @@ for e in range(0, len(arquivos_munic)):
 
     # Gravar dados no banco:
     # munic
-    munic.to_sql(name='munic', con=engine, if_exists='append', index=False)
+    to_sql(munic, name='munic', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_munic[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -645,7 +645,7 @@ for e in range(0, len(arquivos_natju)):
 
     # Gravar dados no banco:
     # natju
-    natju.to_sql(name='natju', con=engine, if_exists='append', index=False)
+    to_sql(natju, name='natju', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_natju[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -690,7 +690,7 @@ for e in range(0, len(arquivos_pais)):
 
     # Gravar dados no banco:
     # pais
-    pais.to_sql(name='pais', con=engine, if_exists='append', index=False)
+    to_sql(pais, name='pais', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_pais[e] + ' inserido com sucesso no banco de dados!')
 
 try:
@@ -735,7 +735,7 @@ for e in range(0, len(arquivos_quals)):
 
     # Gravar dados no banco:
     # quals
-    quals.to_sql(name='quals', con=engine, if_exists='append', index=False)
+    to_sql(quals, name='quals', con=engine, if_exists='append', index=False)
     print('Arquivo ' + arquivos_quals[e] + ' inserido com sucesso no banco de dados!')
 
 try:
