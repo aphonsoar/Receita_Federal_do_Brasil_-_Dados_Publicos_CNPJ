6839b2dd970746d69add5d4d6216fcda51abdbe2
Bruno Peixoto <brunolnetto@gmail.com>
2024-03-07 10:49:20 -0300

    feat/ URLS for receita and Layour

---
+++ code/URLS.py
@@ -0,0 +1,2 @@
+LAYOUT_URL = 'https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/arquivos/NOVOLAYOUTDOSDADOSABERTOSDOCNPJ.pdf'
+DADOS_RF_URL = 'http://200.152.38.155/CNPJ/'
