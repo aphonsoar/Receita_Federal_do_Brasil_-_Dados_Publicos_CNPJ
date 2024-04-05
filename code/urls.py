0d942876fe70331f5c09b9b434e022a900286da9
Bruno Peixoto <brunolnetto@gmail.com>
2024-04-05 12:09:09 -0300

    feat/ improved codebase

--- code/urls.py
+++
@@ -1,2 +0,0 @@
-LAYOUT_URL = 'https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/arquivos/NOVOLAYOUTDOSDADOSABERTOSDOCNPJ.pdf'
-DADOS_RF_URL = 'http://200.152.38.155/CNPJ/'
