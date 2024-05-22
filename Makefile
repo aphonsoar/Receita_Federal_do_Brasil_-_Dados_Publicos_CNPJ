87021a40bfab05508809332d3d3c25cdb534c0ac
Bruno Peixoto <brunolnetto@gmail.com>
2024-05-22 17:42:19 -0300

    config/ requirements, Makefile, scripts

---
+++ Makefile
@@ -0,0 +1,62 @@
+.PHONY: build run stop ps host
+
+OMIT_PATHS := "backend/tests/*"
+
+define PRINT_HELP_PYSCRIPT
+import re, sys
+
+regex_pattern = r'^([a-zA-Z_-]+):.*?## (.*)$$'
+
+for line in sys.stdin:
+	match = re.match(regex_pattern, line)
+	if match:
+		target, help = match.groups()
+		print("%-20s %s" % (target, help))
+endef
+export PRINT_HELP_PYSCRIPT
+
+help:
+	@python -c "$$PRINT_HELP_PYSCRIPT" < $(MAKEFILE_LIST)
+
+clean-logs: # Removes log info. Usage: make clean-logs
+	rm -fr build/ dist/ .eggs/
+	find . -name '*.log' -o -name '*.log' -exec rm -fr {} +
+
+clean-test: # Remove test and coverage artifacts
+	rm -fr .tox/ .testmondata* .coverage coverage.* htmlcov/ .pytest_cache
+
+clean-cache: # remove test and coverage artifacts
+	find . -name '*cache*' -exec rm -rf {} +
+
+sanitize: # Remove dangling images and volumes
+	docker system prune --volumes -f
+	docker images --filter 'dangling=true' -q --no-trunc | xargs -r docker rmi
+
+clean: clean-logs clean-test clean-cache sanitize ## Add a rule to remove unnecessary assets. Usage: make clean
+
+env: ## Creates a virtual environment. Usage: make env
+	pip install virtualenv
+	virtualenv .venv
+
+install: ## Installs the python requirements. Usage: make install
+	uv pip install -r requirements.txt
+
+search: ## Searchs for a token in the code. Usage: make search token=your_token
+	grep -rnw . \
+	--exclude-dir=venv \
+	--exclude-dir=.git \
+	--exclude=poetry.lock \
+	-e "$(token)"
+
+replace: ## Replaces a token in the code. Usage: make replace token=your_token
+	sed -i 's/$(token)/$(new_token)/g' $$(grep -rl "$(token)" . \
+		--exclude-dir=venv \
+		--exclude-dir=.git \
+		--exclude=poetry.lock)
+
+minimal-requirements: ## Generates minimal requirements. Usage: make requirements
+	python3 scripts/clean_packages.py requirements.txt requirements.txt
+
+lint: ## perform inplace lint fixes
+	ruff check --fix .
+
---
+++ scripts/clean_packages.py
@@ -0,0 +1,100 @@
+import re
+from re import escape
+import argparse
+import subprocess
+import os
+
+
+def run_pip_compile(input_file: str, output_file):
+    # Command to run pip-compile
+    command = [
+        "pip-compile",
+        input_file,
+        f"--output-file={output_file}",
+        "--quiet",
+        "--strip-extras",
+    ]
+
+    # Run the command
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running pip-compile: {e}")
+        raise
+
+    # Get package count after operation
+    with open(output_file, "r") as file:
+        lines = file.readlines()
+        package_count = len(lines)
+
+    return package_count
+
+
+def find_packages_with_comment(
+        requirements_file: str,
+        pip_compile_output_file: str,
+        requirements_output_file: str):
+    """
+    Encontra pacores com comentario '# via -r requirements.in' em formato específico.
+    """
+    splitted_file = requirements_file.split('.')
+    name = splitted_file[0]
+    extension = splitted_file[1]
+    pattern = (
+        rf"^([^\s#][\w\-]+)==([\d\.]+)\n\s+# via -r {escape(name)}.{escape(extension)}$"
+    )
+
+    with open(pip_compile_output_file, "r") as file:
+        with open(requirements_output_file, "w") as out_file:
+            text = file.read()
+
+            matches = re.finditer(pattern, text, re.MULTILINE)
+
+            for match in matches:
+                # Catched patterns
+                package_name = match.group(1)
+                version = match.group(2)
+
+                # Write to file
+                out_file.write(package_name + '==' + version + "\n")
+
+    # Get package count after operation
+    with open(requirements_output_file, "r") as file:
+        lines = file.readlines()
+        package_count = len(lines)
+
+    return package_count
+
+
+if __name__ == "__main__":
+    description = "Find packages with a specific comment in a requirements file."
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        "requirements_file", type=str, help="Path to the requirements file."
+    )
+    parser.add_argument(
+        "output_file",
+        type=str,
+        help="Path to the output file.")
+
+    args = parser.parse_args()
+
+    # Get package count before operation
+    with open(args.requirements_file, "r") as file:
+        lines = file.readlines()
+        package_count_before = len(lines)
+
+    # Temporary file for pip-compile output
+    tmp_file = 'tmp.in'
+    run_pip_compile(args.requirements_file, tmp_file)
+
+    # Get package count after operation
+    package_count_after = find_packages_with_comment(
+        args.requirements_file, tmp_file, args.output_file
+    )
+
+    os.remove(tmp_file)
+
+    print(f"Package count before operation: {package_count_before}")
+    print(f"Package count after operation: {package_count_after}")
